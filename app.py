49f016be5a0200692d14a51a297d80a6c6da6fa4
estiven <esti-ven93@hotmail.com>
2022-12-16 13:10:23 -0500

    Se agrego una descripcion del proyecto en la documentacion del api

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from docs import tags_metadata
 from routes.auth import auth_routes
 from routes.user import user_routes
 from routes.vacancy import vacancy_routes
 from starlette.responses import RedirectResponse
 
-app = FastAPI()
+app = FastAPI(
+    title="API HALEK",
+    description="""The HALEK API allows you to perform all the operations you do with our web client.
+
+The HALEK API was built on RESTful principles that ensure predictable URLs that make it easy to build applications. This API follows the rules of HTTP, which allows a wide range of HTTP clients to be used to interact with the API.
+
+Each resource is exposed as a URL. The URL of each resource can be obtained by accessing the API Root Endpoint.""",
+    version="0.0.1",
+    openapi_tags=tags_metadata
+)
+
+
 
 @app.get('/')
 def read_root():
---
+++ docs.py
@@ -0,0 +1,12 @@
+tags_metadata = [
+    {
+        "name": "Module User",
+        "description": "Users routes"
+    },
+
+    {
+        "name": "Module Vacancy",
+        "description": "Vacancyies routes"
+    },
+
+]
